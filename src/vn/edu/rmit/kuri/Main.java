64f98242f28651e1d6ce6fa6129b7347f7a05baa
miketvo <ikxplicitie@gmail.com>
2021-08-02 19:08:14 +0700

    added build artifact configuration

--- src/vn/edu/rmit/kuri/Main.java
+++ src/vn/edu/rmit/kuri/Main.java
@@ -1,8 +1,47 @@
 package vn.edu.rmit.kuri;
 
+import java.io.FileNotFoundException;
+import vn.edu.rmit.kuri.data.Database;
+import vn.edu.rmit.kuri.processing.Summary;
+
 public class Main {
 
+  public static void argError() {
+    System.out.println("Invalid number of arguments");
+  }
+
+  /**
+   * Prints out the program's header. For decoration purpose.
+   * @param databasePath String: Path to the database to be displayed back to the user
+   */
+  public static void header(String databasePath) {
+    System.out.println("╔═══════════════════════════════════════════╗");
+    System.out.println("║               COVID TRACKER               ║");
+    System.out.println("╚═══════════════════════════════════════════╝");
+    System.out.printf("Database Path: %s\n", databasePath);
+  }
+
   public static void main(String[] args) {
-    // Code here
+    if (args.length != 1) {  // Program accepts only one argument: path to the covid database
+      argError();
+    } else {
+      try {
+        String databasePath = args[0];
+        header(databasePath);
+
+        // Load database into memory
+        Database database = new Database(databasePath);
+
+        // Prompt user for input
+
+        // Data processing
+        Summary summary = new Summary();
+
+        // Display processed data
+
+      } catch (FileNotFoundException e) {
+        System.out.println(e.getMessage());
+      }
+    }
   }
 }
--- src/vn/edu/rmit/kuri/output/Display.java
+++ src/vn/edu/rmit/kuri/output/Display.java
@@ -1,5 +1,14 @@
 package vn.edu.rmit.kuri.output;
 
+import vn.edu.rmit.kuri.processing.Summary;
+
 public class Display {
 
+  public static void tabular(Summary summary) {
+    // code here
+  }
+
+  public static void chart(Summary summary) {
+    // code here
+  }
 }
