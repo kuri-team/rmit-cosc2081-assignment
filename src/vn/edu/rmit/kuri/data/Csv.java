12c22385f8c06804db4c31ddf68f3c2a05f8463b
miketvo <ikxplicitie@gmail.com>
2021-08-02 19:38:13 +0700

    added docstring for Csv class

--- src/vn/edu/rmit/kuri/data/Csv.java
+++ src/vn/edu/rmit/kuri/data/Csv.java
@@ -8,10 +8,24 @@ import java.util.Scanner;
 
 public class Csv {
 
+  /**
+   * Reads a .csv file and return it as an <code>ArrayList</code> containing each data row as a
+   * <code>HashMap</code>. <br /><br /><strong>Note:</strong> The 1st row will be considered the
+   * header row and the <code>HashMap</code> keys for subsequent data rows are generated using
+   * values in this row.
+   *
+   * @param path Path to the .csv file to be read
+   * @return <code>ArrayList&lt;HashMap&lt;String, String&gt;&gt;</code>
+   * @throws FileNotFoundException If the path is invalid
+   */
   public static ArrayList<HashMap<String, String>> read(String path) throws FileNotFoundException {
     ArrayList<HashMap<String, String>> data = new ArrayList<>();
-
     Scanner sc = new Scanner(new File(path));
+
+    if (!sc.hasNextLine()) {  // Return an empty ArrayList if the file is empty
+      return data;
+    }
+
     String[] header = sc.nextLine().split(",");
     while (sc.hasNextLine()) {
       HashMap<String, String> row = new HashMap<>();
