18f8fb3d2eb764bda3b4231d57f6b86cbc172f82
rider3458 <s3878480@rmit.edu.vn>
2021-08-06 19:59:09 +0700

    Update Display todos, no longer import javax.swing

--- src/vn/edu/rmit/kuri/output/Display.java
+++ src/vn/edu/rmit/kuri/output/Display.java
@@ -3,18 +3,25 @@ package vn.edu.rmit.kuri.output;
 import vn.edu.rmit.kuri.input.Metric;
 import vn.edu.rmit.kuri.input.ResultType;
 import vn.edu.rmit.kuri.processing.Summary;
-import javax.swing.JTable;
-import javax.swing.JFrame;
-import javax.swing.JScrollPane;
 
 public class Display {
 
   public static void tabular(Summary summary, Metric metric, ResultType resultType) {
     // Dummy content TODO: Implement this
-    // Imported classes for creating table: Jtable, Jframe, JScrollPane
-    // Data from summary will be queried to return a 2D array to add data to the table
-    // metric and resultType will determine the data of the 2D array
-    // The table will 2 have 2 columns: Range and Value
+    // Create a table
+    // Data from summary will be queried to return 2 arrays to add data to the table
+    // The 1st array stores the Range and the 2nd stores the Value data
+    // Elements with the same index from the 2 arrays matches each other
+    // The headers of the table are Range and Value
+    // metric and resultType will determine the data of the arrays
+    // Table demo (the width can be changed):
+    //+---------------------+---------------+
+    //|         Range       |     Value     |
+    //+---------------------+---------------+
+    //|         .....       |     .....     |
+    //|         .....       |     .....     |
+    //|         .....       |     .....     |
+    //+---------------------+---------------+
     System.out.println("To be implemented");
   }
 
