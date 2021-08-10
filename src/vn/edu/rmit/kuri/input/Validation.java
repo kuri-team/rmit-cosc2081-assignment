e14d199680e0762e83c2830ec4cb48fdaa5b27a4
doanyennhi <s3880599@rmit.edu.vn>
2021-08-11 01:07:57 +0700

    Modify checkInput method, remove redundant method

--- src/vn/edu/rmit/kuri/input/Validation.java
+++ src/vn/edu/rmit/kuri/input/Validation.java
@@ -4,15 +4,6 @@ import java.util.Scanner;
 
 public class Validation {
 
-  public static boolean isIntegerFormat(String input) {
-    try {
-      Integer.parseInt(input);
-      return true;
-    } catch (NumberFormatException exception) {
-      return false;
-    }
-  }
-
   public static int returnValidOption(String input, int[] options) {
     for (int option : options) {
       if (input.equals(Integer.toString(option))) {
@@ -22,9 +13,10 @@ public class Validation {
     return -1;
   }
 
-  public static int checkInput(String input, Scanner sc) {
-    while (!isIntegerFormat(input)) {
-      System.out.print("Your input is invalid. Please try again.\n>>>\s");
+  public static int checkInput(String input, Scanner sc, int[] options) {
+    while (returnValidOption(input, options) == -1) {
+      System.out.print("Your input is invalid. Please enter one of the options "
+          + "specified above.\n>>>\s");
       input = sc.nextLine();
     }
     return Integer.parseInt(input);
@@ -32,7 +24,8 @@ public class Validation {
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int option = checkInput("abc", sc);
+    int[] common = {1, 2, 3};
+    int option = checkInput("abc", sc, common);
     System.out.println(option);
   }
 }
