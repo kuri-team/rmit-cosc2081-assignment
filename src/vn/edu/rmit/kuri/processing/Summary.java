65e1ce26e59eabbd00e5ce71e1f8163094873a9e
miketvo <ikxplicitie@gmail.com>
2021-08-14 22:50:37 +0700

    Convert filteredData into a local variable to save memory

--- src/vn/edu/rmit/kuri/processing/Summary.java
+++ src/vn/edu/rmit/kuri/processing/Summary.java
@@ -10,40 +10,43 @@ import vn.edu.rmit.kuri.input.Grouping;
 
 public class Summary implements DataArray<ArrayList<Data>> {
 
-  private final DataFilter filteredData;
   private final ArrayList<ArrayList<Data>> processedData;
 
   public Summary(String geoArea, DateRange dateRange, Grouping grouping, Database database) {
-    this.filteredData = new DataFilter(geoArea, dateRange.getStart(), dateRange.getEnd(), database);
+    DataFilter filteredData = new DataFilter(
+        geoArea,
+        dateRange.getStart(),
+        dateRange.getEnd(),
+        database);
     this.processedData = new ArrayList<>();
 
     switch (grouping.getType()) {
       case NONE -> {
-        for (int i = 0; i < this.filteredData.size(); i++) {
+        for (int i = 0; i < filteredData.size(); i++) {
           ArrayList<Data> group = new ArrayList<>();
-          group.add(this.filteredData.get(i));
+          group.add(filteredData.get(i));
           this.processedData.add(group);
         }
       }
       case N_GROUPS -> {
-        int daysPerGroup = this.filteredData.size() / grouping.getGroupingNum();
+        int daysPerGroup = filteredData.size() / grouping.getGroupingNum();
         int groupCount = 0;
         ArrayList<Data> group = new ArrayList<>();
-        for (int i = 0; i < this.filteredData.size(); i++) {
-          group.add(this.filteredData.get(i));
+        for (int i = 0; i < filteredData.size(); i++) {
+          group.add(filteredData.get(i));
           if ((i + 1) % daysPerGroup == 0 && groupCount < grouping.getGroupingNum() - 1) {
             this.processedData.add(group);
             groupCount++;
             group = new ArrayList<>();
-          } else if (i == this.filteredData.size() - 1) {
+          } else if (i == filteredData.size() - 1) {
             this.processedData.add(group);
           }
         }
       }
       case N_DAYS_PER_GROUP -> {
         ArrayList<Data> group = new ArrayList<>();
-        for (int i = 0; i < this.filteredData.size(); i++) {
-          group.add(this.filteredData.get(i));
+        for (int i = 0; i < filteredData.size(); i++) {
+          group.add(filteredData.get(i));
           if ((i + 1) % grouping.getGroupingNum() == 0) {
             this.processedData.add(group);
             group = new ArrayList<>();
